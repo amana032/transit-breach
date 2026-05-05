40a0c8061276c428121be2acfac7b171cbf14fe5
Benedict Ignacio <btmignacio818@gmail.com>
2026-05-05 15:02:54 -0700

    Updated centipede model

---
+++ centipede_prototype.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+var counter: float = 0.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	counter += delta
+	
