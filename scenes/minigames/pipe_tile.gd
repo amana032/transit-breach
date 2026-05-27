976b4c58cbcd7f125ce31cc57f9ca23910fb9d25
Alexis M <amana032@ucr.edu>
2026-05-26 20:13:43 -0700

    merge

--- scenes/minigames/pipe_tile.gd
+++ scenes/minigames/pipe_tile.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var sprite = get_node("Area2D/Sprite2D")
 var pipe_rotation:= 0
 var type:= 0
+var connected = false
 
 func _ready():
 	type = randi() % 4 # 0: straight, 1: corner, 2: t-junction, 3: cross
@@ -10,6 +11,7 @@ func _ready():
 	rotation = deg_to_rad(float(pipe_rotation) * 90.0)
 	scale = Vector2(0.5, 0.5)
 
+	# Set sprite to correct type
 	match type:
 		0:
 			sprite.texture = preload("res://assets/Pipe Assets/straight-pipe.png")
@@ -25,13 +27,14 @@ func _ready():
 			pass
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			pipe_rotation = (pipe_rotation + 1) % 4
-			rotation = deg_to_rad(float(pipe_rotation) * 90.0)
-			print("Pipe rotated to: ", pipe_rotation)
 
-func update_pipe(new_type: int):
+	# On mouse click, rotate the pipe
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		pipe_rotation = (pipe_rotation + 1) % 4
+		rotation = deg_to_rad(float(pipe_rotation) * 90.0)
+			
+
+func update_pipe(new_type: int, new_connection: bool):
 	match new_type:
 		0:
 			sprite.texture = preload("res://assets/Pipe Assets/straight-pipe.png")
@@ -43,4 +46,5 @@ func update_pipe(new_type: int):
 			sprite.texture = preload("res://assets/Pipe Assets/cross-pipe.png")
 
 	type = new_type
+	self.connected = new_connection
 	
